7fc09d1b269371650651837d8d8f772fc50668b3
ezio <ezio_zhang@outlook.com>
2019-05-27 22:10:10 +0800

    1. update gpio setup;2. update pwm code

--- chapter4/linux/application/4.3/pwmtest.c
+++ chapter4/linux/application/4.3/pwmtest.c
@@ -4,86 +4,111 @@ int main(int argc, char **argv)
 {
     int PwmChannel;
     int MyPeriod = 1000000000; //period 设置为1s
-	float rate = 0.5;
+    float rate = 0.5;
     int MyDuty = MyPeriod * rate;
-	char *array[3] = {"B", "C", "D"};
+    char *array[4] = {"A", "B", "C", "D"};
 
-	/* handle (optional) flags first */
-	if(argc != 2) {
-		fprintf(stderr, "Usage:  %s <PWM Channel Name(B or C or D)>\n", argv[0]);
-		exit(1);
-	}
+    /* handle (optional) flags first */
+    if(argc != 2) {
+        fprintf(stderr, "Usage:  %s <PWM Channel Name(B or C or D)>\n", argv[0]);
+        exit(1);
+    }
+    printf("%s-%s\n",argv[0],argv[1]);
 
-	if(!strcmp(argv[1],array[0])){
-		/*export corresponding PWM Channel*/
-		PwmChannel = 1;
-        if(pwm_export(PwmChannel) < 0){
-        	printf("PWM export error!\n");
-        	return(-1);
+    if(!strcmp(argv[1],array[0])) {
+	    printf("a\n");
+        /*export corresponding PWM Channel*/
+        PwmChannel = 0;
+        if(pwm_export(PwmChannel) < 0) {
+            printf("PWM export error!\n");
+            return(-1);
         }
-        if(pwm_disable(PwmChannel) < 0){
-        	printf("PWM disable error!\n");
-        	return(-1);
+        if(pwm_disable(PwmChannel) < 0) {
+            printf("PWM disable error!\n");
+            return(-1);
         }
         /* set period and duty cycle time in ns */
-        if(pwm_config(PwmChannel, MyPeriod, MyDuty) < 0){
-        	printf("PWM configure error!\n");
-        	return(-1);
+        if(pwm_config(PwmChannel, MyPeriod, MyDuty) < 0) {
+            printf("PWM configure error!\n");
+            return(-1);
         }
         /* enable corresponding PWM Channel */
-        if(pwm_enable(PwmChannel) < 0){
-        	printf("PWM enable error!\n");
-        	return(-1);
+        if(pwm_enable(PwmChannel) < 0) {
+            printf("PWM enable error!\n");
+            return(-1);
         }
-        printf("PWM_B successfully enabled with period - %dms, duty cycle - %2.1f%%\n", MyPeriod/1000000, rate*100);
+        printf("PWM_a successfully enabled with period - %dms, duty cycle - %2.1f%%\n", MyPeriod/1000000, rate*100);
+    } else if(!strcmp(argv[1],array[1])) {
+	    printf("b\n");
+        PwmChannel = 1;
+        if(pwm_export(PwmChannel) < 0) {
+            printf("PWM export error!\n");
+            return(-1);
+        }
+        if(pwm_disable(PwmChannel) < 0) {
+            printf("PWM disable error!\n");
+            return(-1);
+        }
+        /* set period and duty cycle time in ns */
+        if(pwm_config(PwmChannel, MyPeriod, MyDuty) < 0) {
+            printf("PWM configure error!\n");
+            return(-1);
+        }
+        /* enable corresponding PWM Channel */
+        if(pwm_enable(PwmChannel) < 0) {
+            printf("PWM enable error!\n");
+            return(-1);
+        }
+        printf("PWM_b successfully enabled with period - %dms, duty cycle - %2.1f%%\n", MyPeriod/1000000, rate*100);
     }
-	else if(!strcmp(argv[1],array[1])){
-		PwmChannel = 4;
-		if(pwm_export(PwmChannel) < 0){
-			printf("PWM export error!\n");
-			return(-1);
-		}
-		if(pwm_disable(PwmChannel) < 0){
-			printf("PWM disable error!\n");
-			return(-1);
-		}
-		/* set period and duty cycle time in ns */
-		if(pwm_config(PwmChannel, MyPeriod, MyDuty) < 0){
-		printf("PWM configure error!\n");
-		return(-1);
-		}
-		/* enable corresponding PWM Channel */
-		if(pwm_enable(PwmChannel) < 0){
-		printf("PWM enable error!\n");
-		return(-1);
-		}
-		printf("PWM_C successfully enabled with period - %dms, duty cycle - %2.1f%%\n", MyPeriod/1000000, rate*100);
-	}
-	else if(!strcmp(argv[1],array[2])){
-		PwmChannel = 2;
-		if(pwm_export(PwmChannel) < 0){
-		    printf("PWM export error!\n");
-		    return(-1);
-		}
-		if(pwm_disable(PwmChannel) < 0){
-			printf("PWM disable error!\n");
-			return(-1);
-		}
-		/* set period and duty cycle time in ns */
-		if(pwm_config(PwmChannel, MyPeriod, MyDuty) < 0){
-		    printf("PWM configure error!\n");
-			return(-1);
-		}
-		/* enable corresponding PWM Channel */
-		if(pwm_enable(PwmChannel) < 0){
-		printf("PWM enable error!\n");
-		return(-1);
-		}
-		printf("PWM_D successfully enabled with period - %dms, duty cycle - %2.1f%%\n", MyPeriod/1000000, rate*100);
-	}
-	else{
-		printf("wrong PWM Channel input\n");
-		return(-1);
+    else if(!strcmp(argv[1],array[2])) {
+	    printf("c\n");
+        PwmChannel = 2;
+        if(pwm_export(PwmChannel) < 0) {
+            printf("PWM export error!\n");
+            return(-1);
+        }
+        if(pwm_disable(PwmChannel) < 0) {
+            printf("PWM disable error!\n");
+            return(-1);
+        }
+        /* set period and duty cycle time in ns */
+        if(pwm_config(PwmChannel, MyPeriod, MyDuty) < 0) {
+            printf("PWM configure error!\n");
+            return(-1);
+        }
+        /* enable corresponding PWM Channel */
+        if(pwm_enable(PwmChannel) < 0) {
+            printf("PWM enable error!\n");
+            return(-1);
+        }
+        printf("PWM_c successfully enabled with period - %dms, duty cycle - %2.1f%%\n", MyPeriod/1000000, rate*100);
+    } else if(!strcmp(argv[1],array[3])) {
+	    printf("d\n");
+        PwmChannel = 3;
+        if(pwm_export(PwmChannel) < 0) {
+            printf("PWM export error!\n");
+            return(-1);
+        }
+        if(pwm_disable(PwmChannel) < 0) {
+            printf("PWM disable error!\n");
+            return(-1);
+        }
+        /* set period and duty cycle time in ns */
+        if(pwm_config(PwmChannel, MyPeriod, MyDuty) < 0) {
+            printf("PWM configure error!\n");
+            return(-1);
+        }
+        /* enable corresponding PWM Channel */
+        if(pwm_enable(PwmChannel) < 0) {
+            printf("PWM enable error!\n");
+            return(-1);
+        }
+        printf("PWM_d successfully enabled with period - %dms, duty cycle - %2.1f%%\n", MyPeriod/1000000, rate*100);
     }
-	return 0;
+    else{
+	    printf("wrong channel\n");
+	    return -1;
+    }
+    return 0;
 }
--- chapter4/linux/module/gpio_setup/gpio_setup.c
+++ chapter4/linux/module/gpio_setup/gpio_setup.c
@@ -6,9 +6,27 @@ static int __init lkp_init( void )
 {
 printk("Hello,Kernel! \n");
 //mmap  通用配置寄存器 0 0x1fe10420 
+off_t paddr = 0x1fe00000;
+size_t psize = 0x20000;
+uint8_t *vaddr = ioremap(paddr,psize);
+if( vaddr == NULL){
+	printk("ioremap fail 0x%lx\n",paddr);
+}
+else{
+	uint64_t reg0 = *(uint64_t*)(vaddr+0x10420);
+	printk("reg0 0x%llx\n",reg0);
+	reg0 |= 0x3<<10;//set i2c0/1
+	reg0 |= 0xf<<12;//set pwm0-3
+	reg0 &= ~(1<<20);//set 37/38 gpio
+	*(uint64_t*)(vaddr+0x10420) = reg0;
+	reg0 = *(uint64_t*)(vaddr+0x10420);
+	printk("2reg0 0x%llx\n",reg0);
+	iounmap(vaddr);
+}
 //通用配置寄存器 1  0x1fe10428 
 // 通用配置寄存器 2  0x1fe10430 
 //update io pin mux
+
 return 0;
 }
 static void __exit lkp_cleanup( void )
